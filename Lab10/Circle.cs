aee2557c5358ff0f34fc9f887cac2777d1121369
Lin-z Chang <lin-zchang@amrock.com>
2018-10-24 09:39:45 -0400

    Adding files to repo

---
+++ Lab10/Circle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Circle
+    {
+        private double Radius;
+
+        public Circle (double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculcateCircumference()
+        {
+            return (2 * Math.PI * Radius); 
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * (Math.Pow(Radius, 2));
+        }
+
+        private string FormatNumber(double x)
+        {
+            //formats from double to string with 2 decimals
+            return $"{x, 0:F2}";
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            //converts double Circumference to a string with 2 decimal places
+            return FormatNumber(CalculcateCircumference());
+        }
+
+        public string CalculcateFormattedArea()
+        {
+            //converts double Circumference to a string with 2 decimal places
+            return FormatNumber(CalculateArea());
+        }
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Circle Creation Station.");
+            int circleCount = 0;
+
+            while (true)
+            {
+                //prompt user to enter a radius
+                double radius = GetDouble("Enter a radius: ", circleCount);
+                Circle userCircle = new Circle(radius);
+
+                //calculate area and circumference, round to nearest 2 decimals
+                string circumference = userCircle.CalculateFormattedCircumference();
+                string area = userCircle.CalculcateFormattedArea();
+                Console.WriteLine($"The Circumference of your circle is {circumference}, and the Area is {area}.");
+
+                //increases count of objects user has created
+                circleCount++;
+
+                //prompt user to continue
+                string userResponse = GetString("Would you like to make another circle?  Press Y to keep going.");
+
+                if (userResponse.ToUpper() != "Y")
+                {
+                    //display goodbye and number of circles the user built when they quit
+                    Console.WriteLine($"K bye!  You created {circleCount} circles.");
+                    Console.ReadLine();
+                    break;
+                } 
+            }  
+        }
+
+        public static double GetDouble(string message, int circleCount)
+        {
+            double radius; 
+
+            while (true)
+            {
+                Console.Write(message);
+                bool parsed = double.TryParse(Console.ReadLine(), out radius);
+                if (!parsed || radius <= 0)
+                {
+                    Console.WriteLine("That is not valid, please enter a positive number. ");
+                    if (circleCount == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        circleCount--;
+                        continue;
+                    }
+                }
+                else
+                {
+                    return radius;
+                }
+            }
+        }
+
+        public static string GetString(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+    }
+}
